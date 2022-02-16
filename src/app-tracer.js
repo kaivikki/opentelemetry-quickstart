4810aea42046b4540163fb5be4bc1264ba9d354f
vikram-at-911075010171 <arora.vikram0310@gmail.com>
2022-02-16 14:48:56 +0530

    Version 2

--- src/app-tracer.js
+++ src/app-tracer.js
@@ -23,20 +23,63 @@ const initializeTelemetry = (serviceName, metricPort) =>{
   const meter = new MeterProvider({ exporter: metricExporter, interval: 1000 }).getMeter(serviceName);
 
   // Define traces
-  const traceExporter = new JaegerExporter({ endpoint: 'http://localhost:14268/api/traces'});
-  const provider = new NodeTracerProvider({
-      resource: new Resource({
-          [SemanticResourceAttributes.SERVICE_NAME]: serviceName
-      })
+  
+  //1.) Define the Exporter.
+  // Protocols: HTTP, gRPC
+  // Formats : JSON, proto
+  const jaegerExporter = new JaegerExporter({ endpoint: 'http://localhost:14268/api/traces'});
+  const consoleExporter = new ConsoleSpanExporter();
+
+  //2.) Define the resources
+  const serviceResources = serviceSyncDetector.detect();
+  const serviceNameResource =  new Resource({[SemanticResourceAttributes.SERVICE_NAME]: serviceName});
+  const customResource = new Resource({'aws-region':'localhost'});
+
+
+  //3.) Define the samplers
+  const alwaysOnSampler = new AlwaysOffSampler();
+  const paranetBasedSampler = new ParentBasedSampler({
+    root: new TraceIdRatioBasedSampler(1)
   });
-  provider.addSpanProcessor(new SimpleSpanProcessor(traceExporter));
+
+  //4.) Create the Provider with resources and samplers
+  const provider = new NodeTracerProvider({
+    resource: serviceNameResource.merge(customResource),
+    sampler: alwaysOnSampler
+  });
+
+  //5.) Create Processors
+  // SimpleSpanProcessor simply gets a span and ship it to the exporter in this case Jaeger Exporter.
+  const simpleSpanProcessorWithJaegerExporter = new SimpleSpanProcessor(jaegerExporter);
+  const simpleSpanProcessorWithConsoleExporter = new SimpleSpanProcessor(consoleExporter);
+
+  // Batch Span Processor Example. It would buffer the spans.Below example would wait for 7 seconds before shipping the buffered
+  // spans to the exporter.
+  const batchSpanProcessorWithJaegerExporter = new BatchSpanProcessor(jaegerExporter,{
+    scheduledDelayMillis:7000
+  });
+
+  // Add the processor to the provider
+  provider.addSpanProcessor(simpleSpanProcessorWithJaegerExporter);
+
   provider.register();
+  // Register the Instrumentations. Below are example of auto-instrumentation.
+  // Each Instrumentation have its own set of configuration.
   registerInstrumentations({
       instrumentations: [
-          new ExpressInstrumentation(),
+          new ExpressInstrumentation(
+            // Adding Configuration To Express Instrumentation.
+            {
+              requestHook:(span,requestInfo) =>{
+                // This will add the tag called request-header in span with all header details..
+                span.setAttribute('request-headers',JSON.stringify(requestInfo.req.headers))
+              }
+            }
+          ),
           new HttpInstrumentation()
       ]
   });
+
   const tracer = provider.getTracer(serviceName);
   return { meter, tracer };
 };
--- src/item-service.js
+++ src/item-service.js
@@ -23,6 +23,25 @@ app.get('/data', async (request, response) => {
   }
 });
 
+/* Manual Instrumentation Example */
+//setInterval(async () =>{
+  // Below Implementaion will create two traces each having one span. One trace would be Refresh Cache
+  // and second trace would be HTTP GET to mock API. This is not useful as we want this to be a single trace called referesh-cache
+  // two spans together
+  
+  // const span = trace.getTracer('manual').startSpan('Refresh Cache');
+  // const apiResponse = await axios('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8');
+  // span.end();
+  
+
+  // Create a Single Trace with two spans. Anything that happens in callback woule be added to context of startActiveSpan
+ // trace.getTracer('manual').startActiveSpan('Refresh Cache', async (span) =>{
+    //const apiResponse = await axios('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8');
+    //span.end();
+ // })
+
+//},60*1000);
+
 app.listen(8080,()=>{
   console.log('items services is up and running on port 8080');
 });
